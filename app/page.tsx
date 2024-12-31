50b00c15e3a562e62887f3969e8ccd5b3e2bd640
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2025-01-01 01:22:36 +0530

    :sparkles: add math feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import DevToolsLogo from '@/app/ui/dev-tools-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -6,18 +6,13 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
+      <div className="flex h-20 items-end rounded-lg bg-blue-500 p-2 md:h-52">
+        <DevToolsLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
+            <strong>Welcome to Dev Tools.</strong> This site contains tools as per the requirements for developer in his coding adventure.</p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
