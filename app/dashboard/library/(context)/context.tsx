be6d7305b4a5f48ecf79067c270079699688b1ad
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2025-01-05 23:15:53 +0530

    :sparkles: documentation navbar

--- app/dashboard/library/(context)/context.tsx
+++ app/dashboard/library/(context)/context.tsx
@@ -1,54 +1,92 @@
 "use client";
+import { createContext, ReactNode, useContext, useState } from "react";
+import { VscExtensions } from "react-icons/vsc";
 
-import path from "path";
-import { createContext,ReactNode,useContext,useState } from "react";
 
 
-export interface NavigationContext{
-    path:string;
-    push:(relativePath:string)=>void;
-    pop:()=>string;
-    reload:(path:string)=>void;
+export interface Topic_Data {
+  name: string;
+  icon: any;
+  path: string;
+  contents: Content_Data[];
+}
+
+export interface Content_Data {
+  title: string;
+  url: string;
+  subTopic?: Content_Data;
 }
 
 
-interface Props{
-    children:ReactNode
+
+export interface NavigationContext {
+  metaData:[],
+  path: string;
+  push: (relativePath: string) => void;
+  pop: () => string;
+  reload: (path: string) => void;
 }
 
+const list: Topic_Data[] = [
+  {
+    name: "Dev Tools",
+    path: "library/dev_tools",
+    icon: null,
+    contents: [],
+  },
+  {
+    name: "Go Documneter",
+    path: "library/go_documenetor",
+    icon: null,
+    contents: [],
+  },
+  {
+    name: "Vs Code Extension",
+    path: "library/extension",
+    icon: <VscExtensions />,
+    contents: [
+      {
+        title: "Grammar",
+        url: "/grammar",
+      },
+    ],
+  },
+];
 
-const NavigationContext = createContext({});
+export const navigationMetaData:Topic_Data[]=[];
 
-
-export const NavContext=({children}:Props)=>{
-
-    const [path,setPath]=useState('/dashboard/library/');
-
-    const push=(path:string)=>{}
-
-    const pop=()=>{
-        return '';
-    }
-
-    const reload=(path:string)=>{
-
-    }
-
-    let sharedNavigationContext:NavigationContext={
-        path:"/dashboard/library/",
-        push:push,
-        pop:pop,
-        reload:reload
-    }
-
-    return (
-
-        <NavigationContext.Provider value={sharedNavigationContext}>
-            {children}
-        </NavigationContext.Provider>
-    );
+interface Props {
+  children: ReactNode;
 }
 
-export function useNavigationContext(){
-    return useContext(NavigationContext);
-} 
+const NavigationContext = createContext({} as NavigationContext);
+
+export const NavContext = ({ children }: Props) => {
+  const [path, setPath] = useState("/dashboard/library/");
+
+  const push = (path: string) => {};
+
+  const pop = () => {
+    return "";
+  };
+
+  const reload = (path: string) => {};
+
+  let sharedNavigationContext: NavigationContext = {
+    metaData:[],
+    path: "/dashboard/library/",
+    push: push,
+    pop: pop,
+    reload: reload,
+  };
+
+  return (
+    <NavigationContext.Provider value={sharedNavigationContext}>
+      {children}
+    </NavigationContext.Provider>
+  );
+};
+
+export function useNavigationContext() {
+  return useContext(NavigationContext);
+}
--- app/dashboard/library/[slug]/page.tsx
+++ app/dashboard/library/[slug]/page.tsx
@@ -1,8 +1,12 @@
 import { WalkingTheTree } from "@/lib/mdx-utils";
-import fs from "fs"
-import path from "path"
+import metaData from "@/library-database/slugMetaData.json"
 
 
+/**
+ * @description Finds the markdown file from the given list
+ * @param param0 the parameters from url
+ * @returns creates teh mdx from it
+ */
 export default async function Page({
   params,
 }: {
@@ -10,28 +14,22 @@ export default async function Page({
 }) {
   const slug = (await params).slug
 
-  const urls=WalkingTheTree(process.cwd(),'/library-database');
-
-
-  //const { default: Post } = await import(`@/library-database/${slug}.mdx`)
-  console.log({slug})
-  const { default: Post } = await import(`@/library-database/${slug}.mdx`)
+  const { default: Post } = await import(`@/library-database/${slug.split('_').join('/')}.mdx`)
  
   return <Post />
 }
- 
+
+/**
+ * @description shows allowed slugs
+ * @returns {
+ *  params:{
+ *     slug:string
+ *  }
+ * }
+ */
 export async function  generateStaticParams() {
-
-  const files = fs.readdirSync('library-database');
-
-  // return  files.map((file) => {
-  //   return {
-  //   params: {
-  //     slug: file,
-  //   }
-  // }});
-    
-  return WalkingTheTree(process.cwd(),'/library-database')
+ 
+  return JSON.parse(JSON.stringify(metaData))
 }
  
 export const dynamicParams = false
